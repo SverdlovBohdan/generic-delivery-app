eb5e4f9f75a365b01b8a8b4b785f31b61107d6c6
Bohdan Sverdlov <freezar92@gmail.com>
2023-10-30 15:20:41 +0200

    Initial Commit

---
+++ generic-food-delivery-app/generic_food_delivery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  generic_food_delivery_appApp.swift
+//  generic-food-delivery-app
+//
+//  Created by Bohdan Sverdlov on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct generic_food_delivery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
